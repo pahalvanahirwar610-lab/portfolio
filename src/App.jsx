b3d9d91e4301b01ad4ad4a2f558c3ed61097873a
pahalvanahirwar610-lab <pahalvanahirwar610-lab@users.noreply.github.com>
2026-09-09 17:27:35 +0530

    fix Hero import case sensitivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Hero from './components/hero'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import Projects from "./components/Projects"
 import Footer from './components/Footer'
